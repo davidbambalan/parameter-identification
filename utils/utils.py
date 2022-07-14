21cfe09842a8516f80f9e295fac3f8f61ceb5508
davidbambalan <david.bambalan@gmail.com>
2022-07-14 12:19:22 +0800

    Added more utility functions

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,10 @@
 import numpy as np
 import cv2
 
+import numpy as np
+import cv2
+import tkinter.filedialog
+
 def biggestContour(contours):
     # constants
     MIN_AREA = 5000
@@ -46,29 +50,27 @@ def reorder(pts):
 
 def generateCurve(img):
     # constants
-    MIN_AREA = 5000
+    MIN_LENGTH = 1000
     EPSILON_COEFF = 0.001
     IMG_HEIGHT, IMG_WIDTH = img.shape
 
     # declare variables
     biggest = np.array([])
-    maxArea = 0
+    maxLength = 0
 
-    curveContours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    curveContours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in curveContours:
-        contourArea = cv2.contourArea(contour)
-        if (contourArea > MIN_AREA):
-            contourPerimeter = cv2.arcLength(contour, False)
-            contourShape = cv2.approxPolyDP(contour, EPSILON_COEFF * contourPerimeter, False)
-            if (contourArea > maxArea) and (len(contourShape) > 4):
-                biggest = contourShape
-                maxArea = contourArea
+        contourLength = cv2.arcLength(contour, False)
+        if (contourLength > MIN_LENGTH) and (contourLength > maxLength):
+            contourShape = cv2.approxPolyDP(contour, EPSILON_COEFF * contourLength, False)
+            biggest = contourShape
+            maxLength = contourLength
 
     # eliminate outliers
     length, _, _ = biggest.shape
     biggest = biggest.reshape((length, 2))
-    biggest = np.array([[x, y] for [x, y] in biggest if (x > 0.01*IMG_WIDTH and x < 0.99*IMG_WIDTH) and (y > 0.01*IMG_HEIGHT and y < 0.99*IMG_HEIGHT)])
+    biggest = np.array([[x, y] for [x, y] in biggest if (x > 0.05*IMG_WIDTH and x < 0.95*IMG_WIDTH) and (y > 0.05*IMG_HEIGHT and y < 0.95*IMG_HEIGHT)])
 
     return biggest.reshape((len(biggest), 1, 2))
 
@@ -90,3 +92,14 @@ def interpolate(img, pts, xMin=-np.pi, xMax=np.pi, yMin=0, yMax=1):
         newPts.append([x, y])
 
     return np.array(newPts)
+
+def uploadFile():
+    filename = tkinter.filedialog.askopenfilename(
+        initialdir = './',
+        title = 'Select image',
+        filetypes = [
+            ("Image files", "*.png *.jpg *.jpeg")
+        ]
+    )
+
+    return filename
